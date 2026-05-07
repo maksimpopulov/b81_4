6af0a78aeb04f17e6a597c10bb2276e14dc0de2f
maksimpopulov <viktorpopulov@gmail.com>
2026-05-07 13:36:22 +0300

    Add files via upload

---
+++ BugPro/Program.cs
@@ -0,0 +1,155 @@
+﻿using Stateless;
+namespace BugPro;
+
+public enum BugState
+{
+    New,
+    Triaging,
+    NoTime,
+    SeparateSolution,
+    OtherProduct,
+    NeedMoreInfo,
+    Fixing,
+    ProblemSolved,
+    Closed,
+    Reopened
+}
+
+// Действия (триггеры)
+public enum BugTrigger
+{
+    StartTriaging,
+    NoTimeNow,
+    SeparateSolutionReq,
+    OtherProductIssue,
+    RequestMoreInfo,
+    InfoProvided,
+    StartFix,
+    MarkSolved,
+    ConfirmSolved,
+    NotSolved,
+    Close
+}
+
+public class Bug
+{
+    private readonly StateMachine<BugState, BugTrigger> _machine;
+    private StateMachine<BugState, BugTrigger>.TriggerWithParameters<string>? _requestInfoTrigger;
+
+    public BugState State => _machine.State;
+
+    public event Action<BugState, BugState>? StateChanged;
+
+    public Bug()
+    {
+        _machine = new StateMachine<BugState, BugTrigger>(BugState.New);
+        ConfigureMachine();
+    }
+
+    private void ConfigureMachine()
+    {
+        _machine.Configure(BugState.New)
+            .Permit(BugTrigger.StartTriaging, BugState.Triaging);
+        _machine.Configure(BugState.Triaging)
+            .Permit(BugTrigger.NoTimeNow, BugState.NoTime)
+            .Permit(BugTrigger.SeparateSolutionReq, BugState.SeparateSolution)
+            .Permit(BugTrigger.OtherProductIssue, BugState.OtherProduct)
+            .Permit(BugTrigger.RequestMoreInfo, BugState.NeedMoreInfo)
+            .Permit(BugTrigger.StartFix, BugState.Fixing);
+        _machine.Configure(BugState.NoTime)
+            .Permit(BugTrigger.StartTriaging, BugState.Triaging);
+        _machine.Configure(BugState.SeparateSolution)
+            .Permit(BugTrigger.MarkSolved, BugState.ProblemSolved);
+        _machine.Configure(BugState.OtherProduct)
+            .Permit(BugTrigger.Close, BugState.Closed);
+        _machine.Configure(BugState.NeedMoreInfo)
+            .Permit(BugTrigger.InfoProvided, BugState.Triaging)
+            .Permit(BugTrigger.Close, BugState.Closed);
+        _machine.Configure(BugState.Fixing)
+            .Permit(BugTrigger.MarkSolved, BugState.ProblemSolved);
+        _machine.Configure(BugState.ProblemSolved)
+            .Permit(BugTrigger.ConfirmSolved, BugState.Closed)
+            .Permit(BugTrigger.NotSolved, BugState.Reopened);
+        _machine.Configure(BugState.Reopened)
+            .Permit(BugTrigger.StartTriaging, BugState.Triaging)
+            .Permit(BugTrigger.StartFix, BugState.Fixing);
+        _machine.Configure(BugState.Closed)
+            .PermitReentry(BugTrigger.Close);
+
+        _machine.OnTransitioned(transition =>
+        {
+            StateChanged?.Invoke(transition.Source, transition.Destination);
+        });
+    }
+
+    public void Fire(BugTrigger trigger)
+    {
+        if (_machine.CanFire(trigger))
+        {
+            _machine.Fire(trigger);
+        }
+        else
+        {
+            throw new InvalidOperationException($"Cannot fire {trigger} in state {State}");
+        }
+    }
+
+    public bool CanFire(BugTrigger trigger) => _machine.CanFire(trigger);
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var bug = new Bug();
+
+        Console.WriteLine("=== Демонстрация Workflow бага ===\n");
+
+        bug.StateChanged += (from, to) =>
+        {
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Баг перешел из {from} в {to}");
+        };
+
+        Console.WriteLine($"Текущее состояние: {bug.State}");
+
+        bug.Fire(BugTrigger.StartTriaging);
+        Console.WriteLine($"После StartTriaging: {bug.State}");
+
+        bug.Fire(BugTrigger.RequestMoreInfo);
+        Console.WriteLine($"После RequestMoreInfo: {bug.State}");
+
+        bug.Fire(BugTrigger.InfoProvided);
+        Console.WriteLine($"После InfoProvided: {bug.State}");
+
+        bug.Fire(BugTrigger.StartFix);
+        Console.WriteLine($"После StartFix: {bug.State}");
+
+        bug.Fire(BugTrigger.MarkSolved);
+        Console.WriteLine($"После MarkSolved: {bug.State}");
+
+        bug.Fire(BugTrigger.ConfirmSolved);
+        Console.WriteLine($"После ConfirmSolved: {bug.State}");
+
+        Console.WriteLine("\n=== Демонстрация ветки 'Нет времени сейчас' ===");
+
+        var bug2 = new Bug();
+        bug2.StateChanged += (from, to) => Console.WriteLine($"  {from} -> {to}");
+
+        bug2.Fire(BugTrigger.StartTriaging);
+        bug2.Fire(BugTrigger.NoTimeNow);
+        bug2.Fire(BugTrigger.StartTriaging);
+
+        Console.WriteLine("\n=== Попытка недопустимого перехода ===");
+        try
+        {
+            bug.Fire(BugTrigger.NotSolved);
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Ошибка (ожидаемо): {ex.Message}");
+        }
+
+        Console.WriteLine("\nНажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
---
+++ BugTests/UnitTest1.cs
@@ -0,0 +1,207 @@
+﻿using BugPro;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BugTests;
+
+[TestClass]
+public class BugWorkflowTests
+{
+    [TestMethod]
+    public void Test_InitialState_ShouldBeNew()
+    {
+        var bug = new Bug();
+
+        Assert.AreEqual(BugState.New, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_New_To_Triaging_ValidTransition()
+    {
+        var bug = new Bug();
+
+        bug.Fire(BugTrigger.StartTriaging);
+
+        Assert.AreEqual(BugState.Triaging, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_Triaging_To_NoTime_ValidTransition()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+
+        bug.Fire(BugTrigger.NoTimeNow);
+
+        Assert.AreEqual(BugState.NoTime, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_Triaging_To_SeparateSolution_ValidTransition()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+
+        bug.Fire(BugTrigger.SeparateSolutionReq);
+
+        Assert.AreEqual(BugState.SeparateSolution, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_SeparateSolution_To_ProblemSolved_ValidTransition()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.SeparateSolutionReq);
+
+        bug.Fire(BugTrigger.MarkSolved);
+
+        Assert.AreEqual(BugState.ProblemSolved, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_ProblemSolved_To_Closed_WithConfirmation()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.StartFix);
+        bug.Fire(BugTrigger.MarkSolved);
+
+        bug.Fire(BugTrigger.ConfirmSolved);
+
+        Assert.AreEqual(BugState.Closed, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_ProblemSolved_To_Reopened_WhenNotSolved()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.StartFix);
+        bug.Fire(BugTrigger.MarkSolved);
+
+        bug.Fire(BugTrigger.NotSolved);
+
+        Assert.AreEqual(BugState.Reopened, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_Reopened_To_Triaging_Valid()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.StartFix);
+        bug.Fire(BugTrigger.MarkSolved);
+        bug.Fire(BugTrigger.NotSolved);
+
+        bug.Fire(BugTrigger.StartTriaging);
+
+        Assert.AreEqual(BugState.Triaging, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_Reopened_To_Fixing_Valid()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.StartFix);
+        bug.Fire(BugTrigger.MarkSolved);
+        bug.Fire(BugTrigger.NotSolved);
+
+        bug.Fire(BugTrigger.StartFix);
+
+        Assert.AreEqual(BugState.Fixing, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_Triaging_To_NeedMoreInfo_Then_Back()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+
+        bug.Fire(BugTrigger.RequestMoreInfo);
+        Assert.AreEqual(BugState.NeedMoreInfo, bug.State);
+
+        bug.Fire(BugTrigger.InfoProvided);
+        Assert.AreEqual(BugState.Triaging, bug.State);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void Test_InvalidTransition_FromClosed_ToFixing_Throws()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.Close);
+
+        bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.OtherProductIssue);
+        bug.Fire(BugTrigger.Close);
+        bug.Fire(BugTrigger.StartFix);
+    }
+
+    [TestMethod]
+    public void Test_CanFire_BeforeFiring_ReturnsExpected()
+    {
+        var bug = new Bug();
+
+        Assert.IsTrue(bug.CanFire(BugTrigger.StartTriaging));
+        Assert.IsFalse(bug.CanFire(BugTrigger.Close));
+        Assert.IsFalse(bug.CanFire(BugTrigger.StartFix));
+    }
+
+    [TestMethod]
+    public void Test_NoTime_To_Triaging_ValidTransition()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.NoTimeNow);
+        Assert.AreEqual(BugState.NoTime, bug.State);
+
+        bug.Fire(BugTrigger.StartTriaging);
+        Assert.AreEqual(BugState.Triaging, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_OtherProduct_To_Closed_ValidTransition()
+    {
+        var bug = new Bug();
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.OtherProductIssue);
+        Assert.AreEqual(BugState.OtherProduct, bug.State);
+
+        bug.Fire(BugTrigger.Close);
+        Assert.AreEqual(BugState.Closed, bug.State);
+    }
+
+    [TestMethod]
+    public void Test_StateChangedEvent_Fires_OnTransition()
+    {
+        var bug = new Bug();
+        BugState? fromState = null;
+        BugState? toState = null;
+
+        bug.StateChanged += (from, to) =>
+        {
+            fromState = from;
+            toState = to;
+        };
+
+        bug.Fire(BugTrigger.StartTriaging);
+
+        Assert.AreEqual(BugState.New, fromState);
+        Assert.AreEqual(BugState.Triaging, toState);
+    }
+
+    [TestMethod]
+    public void Test_FullHappyPath_NewToClosed()
+    {
+        var bug = new Bug();
+
+        bug.Fire(BugTrigger.StartTriaging);
+        bug.Fire(BugTrigger.StartFix);
+        bug.Fire(BugTrigger.MarkSolved);
+        bug.Fire(BugTrigger.ConfirmSolved);
+
+        Assert.AreEqual(BugState.Closed, bug.State);
+    }
+}
