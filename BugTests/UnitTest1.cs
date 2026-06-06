f37d1a12dd4cbc020ef8e94df7b39dfb784280e0
maksimpopulov <viktorpopulov@gmail.com>
2026-06-06 20:16:51 +0300

    Update UnitTest1.cs

--- BugTests/UnitTest1.cs
+++ BugTests/UnitTest1.cs
@@ -1,133 +1,164 @@
 ﻿﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BugPro;
 
-namespace BugTests
+namespace BugTests;
+
+[TestClass]
+public class BugWorkflowTests
 {
-    [TestClass]
-    public class UnitTest1
+    [TestMethod]
+    public void Test_InitialState_ShouldBeOpen()
     {
-        [TestMethod]
-        public void NewBug_ShouldStartInCreatedState()
-        {
-            var bug = new Bug();
-            Assert.AreEqual(BugState.New, bug.State, "Initial state must be New");
-        }
+        var bug = new Bug();
+        Assert.AreEqual(BugState.Open, bug.State);
+    }
 
-        [TestMethod]
-        public void Triage_ShouldBeReachableFromNew()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            Assert.AreEqual(BugState.Triage, bug.State, "After StartTriage state should be Triage");
-        }
+    [TestMethod]
+    public void Test_Assign_ShouldChangeStateToAssigned()
+    {
+        var bug = new Bug();
+        bug.Assign("TestDeveloper");
+        Assert.AreEqual(BugState.Assigned, bug.State);
+    }
 
-        [TestMethod]
-        public void NeedInfo_ShouldTransitionFromTriage()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.NeedMoreInfo);
-            Assert.AreEqual(BugState.NeedInfo, bug.State);
-        }
+    [TestMethod]
+    public void Test_Assign_ShouldStoreAssigneeName()
+    {
+        var bug = new Bug();
+        string expectedAssignee = "John Developer";
+        bug.Assign(expectedAssignee);
+        Assert.AreEqual(expectedAssignee, bug.AssignedTo);
+    }
 
-        [TestMethod]
-        public void NeedInfo_CanGoBackToTriage()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.NeedMoreInfo);
-            bug.Fire(BugTrigger.ReturnToTriage);
-            Assert.AreEqual(BugState.Triage, bug.State);
-        }
+    [TestMethod]
+    public void Test_StartWork_AfterAssign_ShouldChangeToInProgress()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        Assert.AreEqual(BugState.InProgress, bug.State);
+    }
 
-        [TestMethod]
-        public void AssignToDev_MovesToInProgress()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.AssignToDev);
-            Assert.AreEqual(BugState.InProgress, bug.State);
-        }
+    [TestMethod]
+    public void Test_Fix_AfterStartWork_ShouldChangeToFixed()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        Assert.AreEqual(BugState.Fixed, bug.State);
+    }
 
-        [TestMethod]
-        public void Fix_ChangesStateFromInProgressToFixed()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.AssignToDev);
-            bug.Fire(BugTrigger.Fix);
-            Assert.AreEqual(BugState.Fixed, bug.State);
-        }
+    [TestMethod]
+    public void Test_Verify_AfterFix_ShouldChangeToVerified()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Verify();
+        Assert.AreEqual(BugState.Verified, bug.State);
+    }
 
-        [TestMethod]
-        public void Verify_ClosesFixedBug()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.AssignToDev);
-            bug.Fire(BugTrigger.Fix);
-            bug.Fire(BugTrigger.Verify);
-            Assert.AreEqual(BugState.Closed, bug.State);
-        }
+    [TestMethod]
+    public void Test_Close_AfterVerify_ShouldChangeToClosed()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Verify();
+        bug.Close();
+        Assert.AreEqual(BugState.Closed, bug.State);
+    }
 
-        [TestMethod]
-        public void Reopen_ReturnsToReopenedState()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.AssignToDev);
-            bug.Fire(BugTrigger.Fix);
-            bug.Fire(BugTrigger.Reopen);
-            Assert.AreEqual(BugState.Reopened, bug.State);
-        }
+    [TestMethod]
+    public void Test_Reopen_FromClosed_ShouldChangeToReopened()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Verify();
+        bug.Close();
+        bug.Reopen();
+        Assert.AreEqual(BugState.Reopened, bug.State);
+    }
 
-        [TestMethod]
-        public void ReopenedBug_CanBeAssignedAgain()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.AssignToDev);
-            bug.Fire(BugTrigger.Fix);
-            bug.Fire(BugTrigger.Reopen);
-            bug.Fire(BugTrigger.AssignToDev);
-            Assert.AreEqual(BugState.InProgress, bug.State);
-        }
+    [TestMethod]
+    public void Test_Reject_FromOpen_ShouldChangeToRejected()
+    {
+        var bug = new Bug();
+        bug.Reject();
+        Assert.AreEqual(BugState.Rejected, bug.State);
+    }
 
-        [TestMethod]
-        public void Triage_MarkingNotABug_LeadsToNotABugState()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.MarkNotABug);
-            Assert.AreEqual(BugState.NotABug, bug.State);
-        }
+    [TestMethod]
+    public void Test_Reopen_FromRejected_ShouldChangeToReopened()
+    {
+        var bug = new Bug();
+        bug.Reject();
+        bug.Reopen();
+        Assert.AreEqual(BugState.Reopened, bug.State);
+    }
 
-        [TestMethod]
-        public void NotABug_CanBeClosed()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.MarkNotABug);
-            bug.Fire(BugTrigger.Close);
-            Assert.AreEqual(BugState.Closed, bug.State);
-        }
+    [TestMethod]
+    public void Test_CompleteWorkflow_ShouldEndInClosedState()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        bug.StartWork();
+        bug.Fix();
+        bug.Verify();
+        bug.Close();
+        Assert.AreEqual(BugState.Closed, bug.State);
+    }
 
-        [TestMethod]
-        public void Duplicate_MarkingWorks()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.MarkDuplicate);
-            Assert.AreEqual(BugState.Duplicate, bug.State);
-        }
+    [TestMethod]
+    public void Test_Reassign_FromAssigned_ShouldStayAssigned()
+    {
+        var bug = new Bug();
+        bug.Assign("Dev1");
+        bug.Assign("Dev2");
+        Assert.AreEqual(BugState.Assigned, bug.State);
+    }
 
-        [TestMethod]
-        public void CannotReproduce_StateReachable()
-        {
-            var bug = new Bug();
-            bug.Fire(BugTrigger.StartTriage);
-            bug.Fire(BugTrigger.MarkCannotReproduce);
-            Assert.AreEqual(BugState.CannotReproduce, bug.State);
-        }
+    [TestMethod]
+    public void Test_CannotStartWork_WithoutAssignment()
+    {
+        var bug = new Bug();
+        Assert.ThrowsException<InvalidOperationException>(() => bug.StartWork());
+    }
+
+    [TestMethod]
+    public void Test_CannotFix_WithoutStartingWork()
+    {
+        var bug = new Bug();
+        bug.Assign("Developer");
+        Assert.ThrowsException<InvalidOperationException>(() => bug.Fix());
+    }
+
+    [TestMethod]
+    public void Test_MultipleReopenCloseCycles_ShouldWork()
+    {
+        var bug = new Bug();
+        
+        bug.Assign("Dev1");
+        bug.StartWork();
+        bug.Fix();
+        bug.Verify();
+        bug.Close();
+        Assert.AreEqual(BugState.Closed, bug.State);
+        
+        bug.Reopen();
+        Assert.AreEqual(BugState.Reopened, bug.State);
+        
+        bug.Assign("Dev2");
+        bug.StartWork();
+        bug.Fix();
+        bug.Verify();
+        bug.Close();
+        
+        Assert.AreEqual(BugState.Closed, bug.State);
     }
 }
